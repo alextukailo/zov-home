1211a155299988c6159e00b04e30584ec21468e6
alex <san1295@tut.by>
2021-03-04 16:28:49 +0300

    upd

--- src/js/main.js
+++ src/js/main.js
@@ -440,6 +440,9 @@ var displayAkcii = function displayAkcii() {
         document.getElementById('akcii_secondtitle_output').textContent = item.dataset.title;
         document.getElementById('akcii_desc_output').textContent = item.dataset.description;
         document.getElementById('akcii_expiration_output').textContent = item.dataset.expiration;
+        $('html, body').animate({
+          scrollTop: $('#akcii').position().top - 65
+        }, 'slow');
       };
     });
   }
